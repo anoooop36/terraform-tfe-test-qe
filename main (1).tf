e8e8037cb0438392d7e18857aa89b62f4d64b87a
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-06-29 18:57:14 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -2,7 +2,7 @@ provider "aws" {
   access_key          = "${var.access_key}"
   secret_key          = "${var.secret_key}"
   region              = "${var.region}"
-  #version             = ">= 3.0"
+  version             = ">= 3.0"
 }
 
 resource "aws_subnet" "subnetByTFE" {
